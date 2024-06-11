c7447ec58f7ab56a6dd2a26ba463fb64c04b9b8c
Pathan Adnan Salimkhan <pathanadnan4142@gmail.com>
2024-06-11 11:35:27 +0530

    initial commit

---
+++ adnan7.java
@@ -0,0 +1,9 @@
+class adnan7{
+    public static void main(String[] args){
+        System.out.println("this is java program");
+        System.out.println("output from java program");
+        System.out.println(235);
+        System.out.println(2735);
+        System.out.println("end");
+    }
+}
